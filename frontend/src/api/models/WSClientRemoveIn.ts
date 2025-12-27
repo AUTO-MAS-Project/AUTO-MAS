5140a330ca922efc3478bdef97203034279f7b6a
Alirea <2981883527@qq.com>
2025-12-27 16:16:13 +0800

    feat(API): WS测试API（前端)

---
+++ frontend/src/api/models/WSClientRemoveIn.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * 删除客户端请求
+ */
+export type WSClientRemoveIn = {
+    /**
+     * 客户端名称
+     */
+    name: string;
+};
+
