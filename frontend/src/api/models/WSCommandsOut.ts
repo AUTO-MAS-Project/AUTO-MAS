af2a522f29bc2c0a0f0bdf6a63d8b81e32bfc278
Alirea <2981883527@qq.com>
2025-12-27 16:16:13 +0800

    feat(API): WS测试API（前端)

---
+++ frontend/src/api/models/WSCommandsOut.ts
@@ -0,0 +1,26 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * 可用命令列表响应
+ */
+export type WSCommandsOut = {
+    /**
+     * 状态码
+     */
+    code?: number;
+    /**
+     * 状态
+     */
+    status?: string;
+    /**
+     * 消息
+     */
+    message?: string;
+    /**
+     * 命令列表
+     */
+    data?: (Record<string, any> | null);
+};
+
