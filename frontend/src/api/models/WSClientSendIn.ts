3dc43ff0434b51918f67d17960ecf00c15052608
Alirea <2981883527@qq.com>
2025-12-27 16:16:13 +0800

    feat(API): WS测试API（前端)

---
+++ frontend/src/api/models/WSClientSendIn.ts
@@ -0,0 +1,18 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * 发送消息请求
+ */
+export type WSClientSendIn = {
+    /**
+     * 客户端名称
+     */
+    name: string;
+    /**
+     * 要发送的 JSON 消息
+     */
+    message: Record<string, any>;
+};
+
---
+++ frontend/src/api/models/WSClientStatusOut.ts
@@ -0,0 +1,26 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * 客户端状态响应
+ */
+export type WSClientStatusOut = {
+    /**
+     * 状态码
+     */
+    code?: number;
+    /**
+     * 状态
+     */
+    status?: string;
+    /**
+     * 消息
+     */
+    message?: string;
+    /**
+     * 状态数据
+     */
+    data?: (Record<string, any> | null);
+};
+
