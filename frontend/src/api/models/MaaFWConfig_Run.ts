7f670afd45f2691e10234cf27ed9e9f0a7498468
qiyinxi <qiyinxi@foxmail.com>
2026-08-27 21:10:38 +0200

    chore(api): 重新生成 OpenAPI 客户端，纳入 MaaFW 预览接口与脚本类型

---
+++ frontend/src/api/models/MaaFWConfig_Run.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type MaaFWConfig_Run = {
+    /**
+     * MaaFW 运行引擎
+     */
+    Engine?: string;
+    /**
+     * 运行时间限制（分钟）
+     */
+    RunTimeLimit?: (number | null);
+};
+
